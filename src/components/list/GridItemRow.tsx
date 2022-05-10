87cacde1f49fe06b631cb71553a282b7e6f66a40
paultion0 <1520104320@qq.com>
2022-05-10 15:31:26 +0800

    release: 全部资源、我的审核样式修改

--- src/components/list/GridItemRow.tsx
+++ src/components/list/GridItemRow.tsx
@@ -1,15 +1,17 @@
-import React, { ReactElement } from 'react';
+import React, { CSSProperties, ReactElement } from 'react';
 import { Space } from 'antd';
 interface Props {
   label?: ReactElement | string;
   children?: ReactElement[] | ReactElement | string;
+  childrenStyle?: CSSProperties;
+  childrenTitle?: string
 }
 
-export default React.memo(function GridItemRow({ label, children }: Props): ReactElement {
+export default React.memo(function GridItemRow({ label, children, childrenStyle = {}, childrenTitle='' }: Props): ReactElement {
   return (
     <div className="grid-item-row">
       {label && <div className="grid-item-label">{label}</div>}
-      {children && <div className="grid-item-control">{children}</div>}
+      {children && <div className="grid-item-control" style={childrenStyle} title={childrenTitle}>{children}</div>}
     </div>
   );
 });
